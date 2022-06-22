11827337900875c4f325f4dbd7ee47905001e84f
Emil <emil@MacBook-Pro-Emil.local>
2022-06-22 22:12:14 +0300

    update 22:12

--- src/components/ShaftComponents/ElevatorComponents.vue
+++ src/components/ShaftComponents/ElevatorComponents.vue
@@ -4,10 +4,41 @@
     :style="{
       position: position,
       transform: 'translateY(-' + positionElevator * 100 + 'px)',
-      transition: speed + 's linear',
+      transition: speedTime11 + 's linear',
       height: height,
     }"
-  ></div>
+  >
+    <div v-if="getCallLine.length" class="elevator__scoreboard">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        xmlns:xlink="http://www.w3.org/1999/xlink"
+        viewBox="0 0 512 512"
+        class="elevator__scoreboard_arrow"
+        :class="{ elevator__scoreboard_arrow_up: directionArrowUp }"
+      >
+        <g>
+          <g>
+            <path
+              d="M512,256L247.209,55.793v138.405H0v123.604h247.209v138.405L512,256z M17.658,300.144v-88.289h247.209V91.28L482.721,256
+			L264.867,420.72V300.144H17.658z"
+            />
+          </g>
+        </g>
+        <g>
+          <g>
+            <rect
+              x="275.439"
+              y="294.269"
+              transform="matrix(0.7955 -0.606 0.606 0.7955 -111.4481 275.9842)"
+              width="155.393"
+              height="17.658"
+            />
+          </g>
+        </g>
+      </svg>
+      {{ getCallLine[0] }}
+    </div>
+  </div>
 </template>
 
 <script>
@@ -27,6 +58,7 @@ export default {
     return {
       triggered: true,
       speed: "",
+      lastFloor: "1",
     };
   },
   computed: {
@@ -34,34 +66,53 @@ export default {
       getCallLine: "getCallLine",
     }),
     positionElevator() {
-      return this.getCallLine.length ? this.getCallLine[0] : "1";
+      return this.getCallLine.length ? this.getCallLine[0] : this.lastFloor;
+    },
+    speedTime11() {
+      return Math.abs(this.lastFloor - this.getCallLine[0]);
+    },
+    directionArrowUp() {
+      return this.lastFloor - this.getCallLine[0] < 0 ? true : false;
     },
   },
   methods: {
     speedTime() {
-      this.speed = Math.abs(this.getCallLine[1] - this.getCallLine[0]);
+      this.speed = Math.abs(this.lastFloor - this.getCallLine[0]);
     },
   },
   watch: {
     getCallLine() {
-      if (this.getCallLine.length > 1 && this.triggered) {
+      if (this.getCallLine.length > 0 && this.triggered) {
         this.triggered = false;
+        this.$store.commit("setLastElevatorPosition", 0);
         this.speedTime();
-        this.$store.commit("delCall");
+
         let timer = setInterval(() => {
+          console.log("Interval");
+
+          console.log(
+            " c " +
+              this.lastFloor +
+              " до " +
+              this.getCallLine[0] +
+              " скорость: " +
+              this.speedTime11
+          );
+
           if (this.getCallLine.length == 1) {
-            clearTimeout(timer);
+            this.lastFloor = this.getCallLine[0];
             this.triggered = true;
-          } else {
-            this.speedTime();
             this.$store.commit("delCall");
+            this.$store.commit("setLastElevatorPosition", this.lastFloor);
+            clearTimeout(timer);
+          } else {
+            this.lastFloor = this.getCallLine[0];
+            this.$store.commit("delCall");
+            this.speedTime();
           }
-        }, this.getCallLine[0] * 1000 + 3000);
+        }, this.speed * 1000 + 3000);
       }
     },
-  },
-  mounted() {
-    this.$store.commit("setCall", 1);
   },
 };
 </script>
@@ -72,6 +123,25 @@ export default {
   background: rgba(118, 251, 253, 0.7);
   display: flex;
   justify-content: center;
-  align-items: center;
+  padding-top: 10px;
+
+  &__scoreboard {
+    width: 30px;
+    height: 20px;
+    background-color: rgba(38, 1, 253, 0.3);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    &_arrow {
+      margin-right: 5px;
+      transform: rotate(90deg);
+      transition: all 0.1s linear;
+    }
+
+    &_arrow_up {
+      transform: rotate(-90deg);
+    }
+  }
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -6,21 +6,28 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     callLine: [],
+    lastElevatorPosition: "1"
   },
   getters: {
     getCallLine: (state) => {
       return state.callLine
     },
+    getLastElevatorPosition: (state) => {
+      return state.lastElevatorPosition
+    }
   },
   mutations: {
     setCall(state, payload) {
-      if (!state.callLine.includes(payload)) {
+      if (!state.callLine.includes(payload) && state.lastElevatorPosition !== payload) {
         state.callLine.push(payload)
       }
     },
     delCall(state) {
       state.callLine.shift()
     },
+    setLastElevatorPosition(state, payload) {
+      state.lastElevatorPosition = payload
+    }
   },
   actions: {
   },
