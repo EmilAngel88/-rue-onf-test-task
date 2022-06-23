afeb9fc23894f90102d6b0ae92edb8712ce9bebe
Emil <emil@MacBook-Pro-Emil.local>
2022-06-23 12:22:56 +0300

    localstorage

--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,8 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    callLine: [],
-    lastElevatorPosition: "1"
+    callLine: JSON.parse(localStorage.getItem('callLine') || '[]'),
+    lastElevatorPosition: JSON.parse(localStorage.getItem('lastElevatorPosition') || '1'),
   },
   getters: {
     getCallLine: (state) => {
@@ -20,13 +20,21 @@ export default new Vuex.Store({
     setCall(state, payload) {
       if (!state.callLine.includes(payload) && state.lastElevatorPosition !== payload) {
         state.callLine.push(payload)
+
+        localStorage.setItem('callLine', JSON.stringify(state.callLine))
       }
     },
     delCall(state) {
       state.callLine.shift()
+
+      const removeCalls = JSON.parse(localStorage.getItem('callLine'));
+      removeCalls.shift();
+      localStorage.setItem('callLine', JSON.stringify(removeCalls));
     },
     setLastElevatorPosition(state, payload) {
       state.lastElevatorPosition = payload
+
+      localStorage.setItem('lastElevatorPosition', JSON.stringify(state.lastElevatorPosition))
     }
   },
   actions: {
